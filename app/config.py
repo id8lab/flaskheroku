9e9d806bc69a0cb60e6ffd9f9ae0f20b4d725259
Michael Hansen <michaelhansen@vizneo.com>
2018-12-14 17:50:47 +0800

    heroku config

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ class Config(object):
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     if not app.debug and not app.testing:
         # ...
-
         if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
@@ -14,14 +13,14 @@ class Config(object):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/herokutest.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
+                file_handler = RotatingFileHandler('logs/herokutest.log', 
+                maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(
+                logging.Formatter('%(asctime)s %(levelname)s: %(message)s '
                 '[in %(pathname)s:%(lineno)d]'))
             file_handler.setLevel(logging.INFO)
             app.logger.addHandler(file_handler)
-
         app.logger.setLevel(logging.INFO)
         app.logger.info('Heroku test')
-        return app
+    return app
+    

b78f736544b588f9fd3e0652783483b7b2d675ef
Michael Hansen <michaelhansen@vizneo.com>
2018-12-14 17:52:23 +0800

    heroku config

--- app/config.py
+++ app/config.py
@@ -4,23 +4,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    if not app.debug and not app.testing:
-        # ...
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-                file_handler = RotatingFileHandler('logs/herokutest.log', 
-                maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(
-                logging.Formatter('%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Heroku test')
-    return app
-    
+
+
